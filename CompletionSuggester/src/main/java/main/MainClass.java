bcf670f45b4f08e4651b2ab72ea80925b978960b
Digya053 <digyaacharyaa@gmail.com>
2016-12-30 17:28:58 +0545

    Suggest the next word for a given based on Markov Chain

--- CompletionSuggester/src/main/java/main/MainClass.java
+++ CompletionSuggester/src/main/java/main/MainClass.java
@@ -1,13 +1,18 @@
 package main;
 
+import languagemodel.MarkovChain;
 import languagemodel.Ngram;
+import languagemodel.Probability;
 
 public class MainClass {
 
-	public static void main(String[] args){
-		System.out.println("Hello World");
+	public static void main(String[] args) {
+		Probability probability = new Probability();
 		Ngram ngram = new Ngram();
+		MarkovChain mc = new MarkovChain();
 		ngram.loadFromFile("data/text");
 		ngram.display();
+		System.out.println("The probability is "+ probability.probability(ngram, "It is the"));
+		mc.nextWord(ngram,"of seasons where");
 	}
 }
